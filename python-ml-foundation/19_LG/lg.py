4ab940f2281848b570dd63ad2240979f8991ea09
Anishekh Prasad <anishekprasad@gmail.com>
2026-01-22 21:53:14 +0000

    Jan 23, 2026, 3:23 AM

---
+++ python-ml-foundation/19_LG/lg.py
@@ -0,0 +1,24 @@
+class MeraLin:
+
+    def __init__(self):
+        self.m=None
+        self.b=None
+    
+    def fit(self,x_train,y_train):
+        num=0
+        den=0
+        
+        for i in range(x_train.shape[0]): 
+
+            num=num+((x_train[i]-x_train.mean())*(y_train[i]-y_train.mean()))
+            den=den+((x_train[i]-x_train.mean())*(x_train[i]-x_train.mean()))
+
+        self.m=num/den
+        self.b=y_train.mean()-(self.m* x_train.mean())
+        print(self.m)
+        print(self.b)
+        
+    def predict(self,x_test):
+
+        print(x_test)
+        return self.m * x_test + self.b
